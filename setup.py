ce4f5294256223a2c927b07418009d5bff8e8129
John Benediktsson <mrjbq7@gmail.com>
2012-03-23 12:18:12 -0700

    Update setup with project details.

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
@@ -8,16 +9,34 @@ import sys
 if sys.platform in ("linux2", "darwin"):
     include_talib_dir = "/usr/local/include/ta-lib/"
     lib_talib_dir = "/usr/local/lib/"
+
 elif sys.platform == "win32":
     include_talib_dir = r"c:\msys\1.0\local\include\ta-lib"
-    lib_talib_dir = r"c:\msys\1.0\local\lib"    
-    
+    lib_talib_dir = r"c:\msys\1.0\local\lib"
+
+else:
+    raise NotImplementedError(sys.platform)
+
 ext = Extension("talib", ["talib.pyx"],
-    include_dirs=[numpy.get_include(), 
-                  include_talib_dir],
+    include_dirs=[numpy.get_include(), include_talib_dir],
     library_dirs=[lib_talib_dir],
     libraries=["ta_lib"]
 )
 
-setup(ext_modules=[ext],
-    cmdclass = {'build_ext': build_ext})
+setup(
+    name = 'TA-Lib',
+    version = '0.4.0',
+    description = 'Python wrapper for TA-Lib',
+    author = 'John Benediktsson',
+    author_email = 'mrjbq7@gmail.com',
+    url = 'http://github.com/mrjbq7/ta-lib',
+    download_url = 'http://github.com/mrjbq7/ta-lib/zipball/master#egg=TA-Lib-0.4.0',
+    packages=['talib'],
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "License :: OSI Approved :: BSD License",
+    ],
+    ext_modules=[ext],
+    cmdclass = {'build_ext': build_ext}
+)
