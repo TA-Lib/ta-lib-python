88e62f05af689483ac958b915fbf73ff5e80c38d
John Benediktsson <mrjbq7@gmail.com>
2012-03-23 14:35:26 -0700

    use indices returned from talib.

--- example.py
+++ example.py
@@ -10,16 +10,16 @@ idata = numpy.random.random((TEST_LEN))
 
 i, odata = talib.MA(idata)
 
-i, upper, middle, lower = talib.BBANDS(idata)
+j, upper, middle, lower = talib.BBANDS(idata)
 
-i, kama = talib.KAMA(idata)
+k, kama = talib.KAMA(idata)
 
 pylab.plot(r, idata, 'b-', label="original")
-pylab.plot(r[-len(odata):], odata, 'g-', label="MA")
-pylab.plot(r[-len(upper):], upper, 'r-', label="Upper")
-pylab.plot(r[-len(middle):], middle, 'r-', label="Middle")
-pylab.plot(r[-len(lower):], lower, 'r-', label="Lower")
-pylab.plot(r[-len(kama):], kama, 'g', label="KAMA")
+pylab.plot(r[i:], odata, 'g-', label="MA")
+pylab.plot(r[j:], upper, 'r-', label="Upper")
+pylab.plot(r[j:], middle, 'r-', label="Middle")
+pylab.plot(r[j:], lower, 'r-', label="Lower")
+pylab.plot(r[k:], kama, 'g', label="KAMA")
 pylab.legend()
 pylab.show()
 
