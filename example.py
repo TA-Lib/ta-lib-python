3e785cabd918195fed4138b77744c78a67b2658c
John Benediktsson <mrjbq7@gmail.com>
2012-06-19 16:37:59 -0700

    fixed example which was using old api.

--- example.py
+++ example.py
@@ -9,18 +9,18 @@ r = numpy.arange(TEST_LEN)
 
 idata = numpy.random.random(TEST_LEN)
 
-i, odata = talib.MA(idata)
+odata = talib.MA(idata)
 
-j, upper, middle, lower = talib.BBANDS(idata)
+upper, middle, lower = talib.BBANDS(idata)
 
-k, kama = talib.KAMA(idata)
+kama = talib.KAMA(idata)
 
 pylab.plot(r, idata, 'b-', label="original")
-pylab.plot(r[i:], odata, 'g-', label="MA")
-pylab.plot(r[j:], upper, 'r-', label="Upper")
-pylab.plot(r[j:], middle, 'r-', label="Middle")
-pylab.plot(r[j:], lower, 'r-', label="Lower")
-pylab.plot(r[k:], kama, 'g', label="KAMA")
+pylab.plot(r, odata, 'g-', label="MA")
+pylab.plot(r, upper, 'r-', label="Upper")
+pylab.plot(r, middle, 'r-', label="Middle")
+pylab.plot(r, lower, 'r-', label="Lower")
+pylab.plot(r, kama, 'g', label="KAMA")
 pylab.legend()
 pylab.show()
 
