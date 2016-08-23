e71e1f17d57dd2e9c7073738db546b017612e102
mckelvin <mckelvin@users.noreply.github.com>
2016-08-23 16:40:59 +0800

    Use ta-lib from vendor if not install

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import glob
 import os
 import warnings
 
@@ -41,8 +42,10 @@ else:
 
 from distutils.extension import Extension
 
-lib_talib_name = 'ta_lib'  # the underlying C library's name
 
+lib_talib_name = 'ta_lib'  # the underlying C library's name
+sources = []
+libraries = []
 platform_supported = False
 for prefix in ['darwin', 'linux', 'bsd', 'sunos']:
     if prefix in sys.platform:
@@ -94,34 +97,78 @@ for lib_talib_dir in lib_talib_dirs:
             break
     except OSError:
         pass
+    libraries = [lib_talib_name]
 else:
-    warnings.warn('Cannot find ta-lib library, installation may fail.')
+    warnings.warn(
+        "Cannot find ta-lib library, Try to build from source. "
+        "Installation may fail."
+    )
+    # find vendor/ta-lib -name "*.h" -exec dirname {} \; | sort | uniq
+    vendor_dir = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        "vendor",
+    )
+    vendor_include_dir = os.path.join(
+        vendor_dir,
+        "include",
+    )
+    vendor_talib_dir = os.path.join(
+        vendor_dir,
+        "ta-lib",
+    )
+    talib_include_dirs = [
+        ("include", ),
+        ("src", "ta_abstract"),
+        ("src", "ta_abstract", "frames"),
+        ("src", "ta_common"),
+        ("src", "ta_func"),
+    ]
+    include_dirs.append(os.path.join(vendor_include_dir))
+    include_dirs.extend((
+        os.path.join(vendor_talib_dir, *path_args)
+        for path_args in talib_include_dirs
+    ))
+
+    talib_source_dirs = [
+        ("ta_abstract", ),
+        ("ta_abstract", "frames"),
+        ("ta_abstract", "tables"),
+        ("ta_common", ),
+        ("ta_func", )
+    ]
+    for path_args in talib_source_dirs:
+        source_dir = os.path.join(vendor_talib_dir, "src", *path_args)
+        sources.extend(glob.glob(os.path.join(source_dir, "*.c")))
+    sources.remove(
+        os.path.join(vendor_talib_dir, "src", "ta_abstract", "excel_glue.c")
+    )
+    libraries = []
+    lib_talib_dirs = []
 
 cmdclass = {"test": PyTest}
 if has_cython:
     cmdclass['build_ext'] = build_ext
 
-ext_modules = []
-for name in ['common', 'func', 'abstract', 'stream']:
-    ext = Extension(
-        'talib.%s' % name,
-        [('talib/%s.pyx' if has_cython else 'talib/%s.c') % name],
-        include_dirs = include_dirs,
-        library_dirs = lib_talib_dirs,
-        libraries = [lib_talib_name]
+ext_modules = [
+    Extension(
+        'talib.libta_lib',
+        ["talib/libta_lib.pyx"] + sources,
+        include_dirs=include_dirs,
+        library_dirs=lib_talib_dirs,
+        libraries=libraries
     )
-    ext_modules.append(ext)
+]
 
 
 setup(
-    name = 'TA-Lib',
-    version = '0.4.10',
-    description = 'Python wrapper for TA-Lib',
-    author = 'John Benediktsson',
-    author_email = 'mrjbq7@gmail.com',
-    url = 'http://github.com/mrjbq7/ta-lib',
-    download_url = 'https://github.com/mrjbq7/ta-lib/releases',
-    classifiers = [
+    name='TA-Lib',
+    version='0.4.10',
+    description='Python wrapper for TA-Lib',
+    author='John Benediktsson',
+    author_email='mrjbq7@gmail.com',
+    url='http://github.com/mrjbq7/ta-lib',
+    download_url='https://github.com/mrjbq7/ta-lib/releases',
+    classifiers=[
         "License :: OSI Approved :: BSD License",
         "Development Status :: 4 - Beta",
         "Operating System :: Unix",
@@ -138,8 +185,8 @@ setup(
         "Intended Audience :: Science/Research",
         "Intended Audience :: Financial and Insurance Industry",
     ],
-    packages = ['talib'],
-    ext_modules = ext_modules,
-    cmdclass = cmdclass,
-    requires = ['numpy'],
+    packages=['talib'],
+    ext_modules=ext_modules,
+    cmdclass=cmdclass,
+    requires=['numpy'],
 )
