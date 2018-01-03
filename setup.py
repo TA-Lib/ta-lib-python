986deecb2d56ecccea4ab301f8d1d56595f9f8eb
John Benediktsson <mrjbq7@gmail.com>
2018-01-03 10:50:32 -0800

    setup.py: adding Python 3.6 to metadata.

--- setup.py
+++ setup.py
@@ -117,6 +117,7 @@ setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Cython",
         "Topic :: Office/Business :: Financial",
         "Topic :: Scientific/Engineering :: Mathematics",
