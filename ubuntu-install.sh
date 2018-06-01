36e9180e7c7070b93041e3169579d4b06c173fc2
K-Kit <kylekitlinski@gmail.com>
2018-06-01 13:48:39 -0400

    fixed suggestions

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -11,29 +11,27 @@ echo "|        '##'                                      |"
 echo "----------------------------------------------------"
 
 apt update
-yes | sudo apt-get install gcc build-essential python3-distutils python3-dev
-sudo pip3 install wheel		
-sudo pip3 install regex	
-sudo wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+yes | apt-get install gcc build-essential python3-distutils python3-dev python3-pip
 
-sudo tar -zxvf ta-lib-0.4.0-src.tar.gz
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
 
-sudo rm ta-lib-0.4.0-src.tar.gz
+tar -zxvf ta-lib-0.4.0-src.tar.gz
+
+rm ta-lib-0.4.0-src.tar.gz
 
 cd ta-lib
 
 if [[ `id -un` == "root" ]]
     then
         ./configure
-        sudo make
-        sudo make install
+        make
+        make install
     else
         ./configure --prefix=/usr
-        sudo make
-        sudo make install
+        make
+        make install
 fi
 
-sudo bash -c "/usr/local/lib >> /etc/ld.so.conf"
-sudo /sbin/ldconfig
 
-yes | sudo -H pip3 install Ta-LIB
+yes | pip3 install setuptools
+yes | pip3 install Ta-LIB
