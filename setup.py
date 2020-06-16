7d45d35abec45565d6448ae006dda555efafce17
John Benediktsson <mrjbq7@gmail.com>
2020-06-16 10:49:07 -0700

    update to (unreleased) 0.4.19.

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ ext_modules = [
 
 setup(
     name = 'TA-Lib',
-    version = '0.4.18',
+    version = '0.4.19',
     description = 'Python wrapper for TA-Lib',
     author = 'John Benediktsson',
     author_email = 'mrjbq7@gmail.com',
