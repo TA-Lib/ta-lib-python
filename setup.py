f1a26c2ee2035960ff024a18dcabac7cd5a0f65f
John Benediktsson <mrjbq7@gmail.com>
2020-12-13 11:24:23 -0800

    setup.py: adding python 3.9.

--- setup.py
+++ setup.py
@@ -159,6 +159,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Cython",
         "Topic :: Office/Business :: Financial",
         "Topic :: Scientific/Engineering :: Mathematics",
