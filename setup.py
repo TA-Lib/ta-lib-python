fe511dc48879981ae041be7fe78c6b065d9cc49c
John Benediktsson <mrjbq7@gmail.com>
2021-07-14 13:30:36 -0700

    Version bump to unreleased 0.4.22 for development.

--- setup.py
+++ setup.py
@@ -144,7 +144,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='TA-Lib',
-    version='0.4.21',
+    version='0.4.22',
     description='Python wrapper for TA-Lib',
     long_description=long_description,
     long_description_content_type='text/markdown',
