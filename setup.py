fbc436ba372d76063ccbe3d4d25e54945af40383
John Benediktsson <mrjbq7@gmail.com>
2021-12-13 22:15:59 -0800

    setup.py: Rearrange platform code.
    Allow TA_INCLUDE_PATH and TA_LIBRARY_PATH to be used on all platforms.

--- setup.py
+++ setup.py
@@ -20,44 +20,42 @@ except ImportError:
 
 lib_talib_name = 'ta_lib'  # the underlying C library's name
 
-runtime_lib_dirs = []
-
 platform_supported = False
-for prefix in ['darwin', 'linux', 'bsd', 'sunos']:
-    if prefix in sys.platform:
-        platform_supported = True
-        include_dirs = [
-            '/usr/include',
-            '/usr/local/include',
-            '/opt/include',
-            '/opt/local/include',
-            '/opt/homebrew/include',
-            '/opt/homebrew/opt/ta-lib/include',
-        ]
-        if 'TA_INCLUDE_PATH' in os.environ:
-            include_dirs.append(os.environ['TA_INCLUDE_PATH'])
-        lib_talib_dirs = [
-            '/usr/lib',
-            '/usr/local/lib',
-            '/usr/lib64',
-            '/usr/local/lib64',
-            '/opt/lib',
-            '/opt/local/lib',
-            '/opt/homebrew/lib',
-            '/opt/homebrew/opt/ta-lib/lib',
-        ]
-        if 'TA_LIBRARY_PATH' in os.environ:
-            runtime_lib_dirs = os.environ['TA_LIBRARY_PATH']
-            if runtime_lib_dirs:
-                runtime_lib_dirs = runtime_lib_dirs.split(os.pathsep)
-                lib_talib_dirs.extend(runtime_lib_dirs)
-        break
 
-if sys.platform == "win32":
+if any(s in sys.platform for s in ['darwin', 'linux', 'bsd', 'sunos']):
+    platform_supported = True
+    include_dirs = [
+        '/usr/include',
+        '/usr/local/include',
+        '/opt/include',
+        '/opt/local/include',
+        '/opt/homebrew/include',
+        '/opt/homebrew/opt/ta-lib/include',
+    ]
+    library_dirs = [
+        '/usr/lib',
+        '/usr/local/lib',
+        '/usr/lib64',
+        '/usr/local/lib64',
+        '/opt/lib',
+        '/opt/local/lib',
+        '/opt/homebrew/lib',
+        '/opt/homebrew/opt/ta-lib/lib',
+    ]
+
+elif sys.platform == "win32":
     platform_supported = True
     lib_talib_name = 'ta_libc_cdr'
     include_dirs = [r"c:\ta-lib\c\include"]
-    lib_talib_dirs = [r"c:\ta-lib\c\lib"]
+    library_dirs = [r"c:\ta-lib\c\lib"]
+
+if 'TA_INCLUDE_PATH' in os.environ:
+    paths = os.environ['TA_INCLUDE_PATH'].split(os.pathsep)
+    include_dirs.extend(path for path in paths if path)
+
+if 'TA_LIBRARY_PATH' in os.environ:
+    paths = os.environ['TA_LIBRARY_PATH'].split(os.pathsep)
+    library_dirs.extend(path for path in paths if path)
 
 if not platform_supported:
     raise NotImplementedError(sys.platform)
@@ -68,9 +66,9 @@ try:
 except ImportError:
     has_cython = False
 
-for lib_talib_dir in lib_talib_dirs:
+for path in library_dirs:
     try:
-        files = os.listdir(lib_talib_dir)
+        files = os.listdir(path)
         if any(lib_talib_name in f for f in files):
             break
     except OSError:
@@ -135,9 +133,9 @@ ext_modules = [
         'talib._ta_lib',
         ['talib/_ta_lib.pyx' if has_cython else 'talib/_ta_lib.c'],
         include_dirs=include_dirs,
-        library_dirs=lib_talib_dirs,
+        library_dirs=library_dirs,
         libraries=[lib_talib_name],
-        runtime_library_dirs=runtime_lib_dirs)
+        runtime_library_dirs=library_dirs)
 ]
 
 from os import path
