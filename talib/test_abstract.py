aff2469cbd744361d3ba38180f1aba77c29e8472
John Benediktsson <mrjbq7@gmail.com>
2022-09-06 20:15:36 -0700

    adding a test for the threading issue.

--- talib/test_abstract.py
+++ talib/test_abstract.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pytest
+import copy
 import re
+import time
+import threading
 
 try:
     from collections import OrderedDict
@@ -317,3 +320,49 @@ def test_call_first_exception():
     output = abstract.SMA(inputs, timeperiod=2)
     expected = np.array([np.nan, 1.5, 2.5])
     assert_np_arrays_equal(expected, output)
+
+def test_threading():
+    import pandas as pd
+    TEST_LEN_SHORT = 999
+    TEST_LEN_LONG = 4005
+    LOOPS = 1000
+    THREADS = 4
+
+    data_short = np.random.rand(TEST_LEN_SHORT, 5)
+    data_long = np.random.rand(TEST_LEN_LONG, 5)
+
+    df_short = pd.DataFrame(data_short, columns={
+                            'open', 'high', 'low', 'close', 'volume'})
+    df_long = pd.DataFrame(data_long, columns={
+                           'open', 'high', 'low', 'close', 'volume'})
+
+    total = 0
+
+    def loop():
+        nonlocal total
+        if threading.get_native_id() % 2 == 0:
+            df = copy.deepcopy(df_short)
+        else:
+            df = copy.deepcopy(df_long)
+
+        for _ in range(LOOPS):
+            total += 1
+            df['RSI'] = abstract.RSI(df)
+
+    t0 = time.time()
+
+    threads = []
+    for i in range(THREADS):
+        t = threading.Thread(target=loop)
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()
+
+    t1 = time.time()
+    print('test_len: %d, loops: %d' % (TEST_LEN_LONG, LOOPS))
+    print('%.6f' % (t1 - t0))
+    print('%.6f' % ((t1 - t0) / LOOPS))
+
+    assert total == THREADS * LOOPS
