61fd69ff5990931dd2d3b7291bfdd07683b6e199
Stefan Jansen <stefan@applied-ai.com>
2022-11-16 18:26:08 -0500

    test pypi run

--- tools/install_talib_ubuntu.sh
+++ tools/install_talib_ubuntu.sh
@@ -6,7 +6,4 @@ curl -L -o ta-lib-0.4.0-src.tar.gz http://prdownloads.sourceforge.net/ta-lib/ta-
   ./configure &&
   make &&
   make install &&
-  ldconfig &&
-  yum install -y libtiff-devel libjpeg-devel openjpeg2-devel zlib-devel \
-    freetype-devel lcms2-devel libwebp-devel tcl-devel tk-devel \
-    harfbuzz-devel fribidi-devel libraqm-devel libimagequant-devel libxcb-devel
+  ldconfig
