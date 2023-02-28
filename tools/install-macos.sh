17aa730ab9167855a5c3b79e3418abb0a55f3a12
Donovan Dikaio <dikaio@users.noreply.github.com>
2023-02-28 00:45:52 -0800

    Set path for Apple Silicon architectures
    Check if OS is of Apple Silicon architecture to prep pip install with the correct path.

--- tools/install-macos.sh
+++ tools/install-macos.sh
@@ -1,4 +1,10 @@
 
 brew install ta-lib
 
+arch="$(uname -m)"
+if [ "$arch" = "arm64" ]; then
+  export TA_INCLUDE_PATH="$(brew --prefix ta-lib)/include"
+  export TA_LIBRARY_PATH="$(brew --prefix ta-lib)/lib"
+fi
+
 pip3 install ta-lib
