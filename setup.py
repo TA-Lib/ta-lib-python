caa05b60e956e87434b460d5e50bc706dc199279
John Benediktsson <mrjbq7@gmail.com>
2023-07-13 06:45:13 -0700

    setup.py: add python 3.10, 3.11

--- setup.py
+++ setup.py
@@ -172,6 +172,8 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Cython",
         "Topic :: Office/Business :: Financial",
         "Topic :: Scientific/Engineering :: Mathematics",
