84ac2ccca2c803b83507c2bd4f29a11aba81dc24
jstzwj <1103870790@qq.com>
2025-01-09 04:08:09 +0800

    bug fix

--- tools/build_wheel_windows.cmd
+++ tools/build_wheel_windows.cmd
@@ -11,13 +11,7 @@ set CMAKE_CONFIGURATION_TYPES=Release
 curl -L -o talib-c.zip https://github.com/TA-Lib/ta-lib/archive/refs/tags/v%TALIB_C_VER%.zip
 if errorlevel 1 exit /B 1
 
-curl -L -o talib-python.zip https://github.com/TA-Lib/ta-lib-python/archive/refs/tags/TA_Lib-%TALIB_PY_VER%.zip
-if errorlevel 1 exit /B 1
-
 tar -xzvf talib-c.zip
-if errorlevel 1 exit /B 1
-
-tar -xf talib-python.zip --strip-components=1
 if errorlevel 1 exit /B 1
 
 :: git apply --verbose --binary talib.diff
