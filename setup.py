2ffcf42b8313559a72982ee564bfdb12183e8876
John Benediktsson <mrjbq7@gmail.com>
2025-02-08 20:51:50 -0800

    possibly fix issues building against numpy2

--- setup.py
+++ setup.py
@@ -151,7 +151,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='TA-Lib',
-    version='0.6.2',
+    version='0.6.3',
     description='Python wrapper for TA-Lib',
     long_description=long_description,
     long_description_content_type='text/markdown',
